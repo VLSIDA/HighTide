3f18e7c40d2f6ecc9f65d7add3d3a9c64d78d1bd
Matthew Guthaus <mrg@ucsc.edu>
2026-03-01 07:52:25 -0800

    Fix the M7 width to be valid.

--- designs/asap7/gemmini/pdn.tcl
+++ designs/asap7/gemmini/pdn.tcl
@@ -19,7 +19,7 @@ add_pdn_stripe -grid {top} -layer {M1} -width {0.018} -pitch {0.54} -offset {0} 
 add_pdn_stripe -grid {top} -layer {M2} -width {0.018} -pitch {0.54} -offset {0} -followpins
 add_pdn_stripe -grid {top} -layer {M5} -width {0.12}  -spacing {0.072} -pitch {2.7} -offset {0.300}
 add_pdn_stripe -grid {top} -layer {M6} -width {0.288} -spacing {0.096} -pitch {2.7} -offset {0.513}
-add_pdn_stripe -grid {top} -layer {M7} -width {0.40}  -spacing {0.10}  -pitch {4.0} -offset {0.500}
+add_pdn_stripe -grid {top} -layer {M7} -width {0.416} -spacing {0.10}  -pitch {4.0} -offset {0.500}
 add_pdn_stripe -grid {top} -layer {M8} -width {0.40}  -spacing {0.10}  -pitch {4.0} -offset {0.500}
 add_pdn_stripe -grid {top} -layer {M9} -width {0.80}  -spacing {0.20}  -pitch {8.0} -offset {1.000}
 add_pdn_connect -grid {top} -layers {M1 M2}
